95a2cb6dde9c05245e49d9f719157c9a2fbf3018
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2022-02-03 14:59:32 -0300

    patch de correção 5

--- pages/login/style.js
+++ pages/login/style.js
@@ -90,34 +90,6 @@ const LoginScreenArea = styled.div`
   }
 `
 
-const LogoArea = styled.section`
-  grid-area: logoArea;
-  background-color: ${(props) => props.theme.colors.backgroundTertiary};
-  border-radius: ${(props) => props.theme.border.commonRadius};
-  padding: var(--gutter);
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-  min-height: 263px;
-  @media (min-width: 860px) {
-    min-height: 368px;
-  }
-  p {
-    font-size: 12px;
-    line-height: 1.2;
-    &:not(:last-child) {
-      margin-bottom: 12px;
-    }
-    strong {
-      color: ${(props) => props.theme.colors.colorQuartenary};
-    }
-  }
-  img {
-    max-height: 45px;
-    margin-bottom: 36px;
-  }
-`
-export default { LoginScreenArea, LogoArea }
+
+export default LoginScreenArea
+
